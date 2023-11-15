d14ab216fbd5b3d77e7b0c0dac1154ecf84ba709
noah305 <nourahmad305@hotmail.com>
2023-11-15 19:23:33 +0200

    Added Programs section

--- js/index.js
+++ js/index.js
@@ -1,13 +1,3 @@
-// var prevScrollpos = window.scrollY;
-// window.onscroll = function() {
-// var currentScrollPos = window.scrollY;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("navbar").style.top = "0";
-//   } else {
-//     document.getElementById("navbar").style.top = "-50px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
 
 
 let nav_box= document.getElementById("nav-box");
@@ -18,29 +8,33 @@ let hamburger_close_button=document.getElementById("hamburger-close-button");
 hamburge_button.onclick = function(){
     nav_box.classList.toggle("open-nav");
 
-    // if(nav_box.classList.contains("open-nav")){
-    //     hamburge_button.src='../assets/icons/close_img.svg'
-    // }
-    // else{
-    //     hamburge_button.src='../assets/icons/hamburger_img.svg'
-
-    // }
-
-
 }
 
 hamburger_close_button.onclick = function(){
     nav_box.classList.toggle("open-nav");
 
-    // if(nav_box.classList.contains("open-nav")){
-    //     hamburger_close_button.src='../assets/icons/close_img.svg'
-    // }
-    // else{
-    //     hamburger_close_button.src='../assets/icons/hamburger_img.svg'
-
-    // }
-
 }   
 
 
+function showContent(evt, program_name) {
+    let i, program, tablinks;
+    program = document.getElementsByClassName("program-content");
+    for (i = 0; i < program.length; i++) {
+      program[i].style.display = "none";
+   
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < program.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" tablink-active", "");
+    }
+    document.getElementById(program_name).style.display = "block";
+    evt.currentTarget.className += " tablink-active";
+  }
 
+// document.getElementById("FCS").addEventListener('click', function (){
+//     // document.getElementsByClassName("program-content")[0].classList.toggle('hide-content')
+//     document.getElementsByClassName("program-content")[0].setAttribute('class', 'hide-ul')
+//     tasks_list_ongoing.setAttribute('class', 'hide-ul')
+//     tasks_list_finished.removeAttribute('class', 'hide-ul')
+
+// } )
